ffad9294411ce3cac0b473537d4c879db027e399
Hesnan Ávila <avilahesnan@outlook.com>
2024-04-29 11:49:36 -0300

    feat: add components initials

---
+++ src/http/index.ts
@@ -0,0 +1,14 @@
+export function obterCategorias() {
+    return [
+        {
+            "nome": "Laticínios e Ovos",
+            "ingredientes": ["Ovos", "Queijo", "Leite", "Manteiga", "Creme de Leite", "Iogurte", "Leite Condensado", "Sorverte"],
+            "rotulo": "laticinios_e_ovos"
+        },
+        {
+            "nome": "Farinhas e Fermentos",
+            "ingredientes": ["Farinha de Trigo", "Polvilho", "Farinha de Rosca", "Canjica", "Farinha de Mandioca", "Fubá", "Linhaça", "Fermento Químico"],
+            "rotulo": "farinhas_e_fermentos"
+        }
+    ]
+}
